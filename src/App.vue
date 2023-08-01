a1ed6d9cad01a18c419c1e235887a9f131d03d99
Brandon Lozada <brandon_lozada@outlook.com>
2023-07-31 18:57:49 -0600

    Cambiar espaciado; Subir imágenes.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
+import Header from "./components/Header.vue";
 </script>
 
 <template>
-  <h1>Desde pinia</h1>
+	<Header />
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <header class="mx-auto container px-5 py-16">
+        <div>
+
+
+        </div>
+
+        <form
+            class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"
+        >
+
+        </form>
+    </header>
+</template>
