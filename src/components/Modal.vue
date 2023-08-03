00a0f740e66375e782d419a09e4dd9b10ee56433
BrandonLozada <brandon_lozada@outlook.com>
2023-08-03 01:42:32 -0600

    Subir cambios

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -5,6 +5,25 @@ import { useBebidasStore } from '../stores/bebidas'
 
 const modal = useModalStore()
 const bebidas = useBebidasStore()
+
+const formatearIngredientes = () => {
+    const ingredientesDiv = document.createElement('DIV')
+
+    for(let i = 1; i <= 15; i++) {
+        if(bebidas.receta[`strIngredient${i}`]) {
+            const ingrediente = bebidas.receta[`strIngredient${i}`]
+            const cantidad = bebidas.receta[`strMeasure${i}`]
+
+            const ingredienteCantidad = document.createElement('P')
+            ingredienteCantidad.classList.add('text-lg')
+            ingredienteCantidad.textContent = `${ingrediente} - ${cantidad ? cantidad : 'Al gusto'}`
+
+            ingredientesDiv.appendChild(ingredienteCantidad)
+        }
+    }
+
+    return ingredientesDiv
+}
 </script>
 
 <template>
@@ -32,8 +51,20 @@ const bebidas = useBebidasStore()
                                     <img
                                         :src="bebidas.receta.strDrinkThumb"
                                         :alt="`Imagen de ${bebidas.receta.strDrink}`"
+                                        class="mx-auto w-96"
                                     />
 
+                                    <DialogTitle as="h3" class="text-gray-900 text-4xl font-extrabold my-5">
+                                        Ingredientes y Cantidad
+                                    </DialogTitle>
+
+                                    <div v-html="formatearIngredientes().outerHTML"></div>
+
+                                    <DialogTitle as="h3" class="text-gray-900 text-4xl font-extrabold my-5">
+                                        Instrucciones
+                                    </DialogTitle>
+
+                                    <p class="text-lg text-gray-500">{{ bebidas.receta.strInstructions }}</p>
 
 
                                 </div>
